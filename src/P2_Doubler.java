13b5723501a14f6d5b208d18818ed6910a0d8b51
mshelby1 <mshelby1@live.maryville.edu>
2018-04-02 11:29:18 -0500

    mshelby1

--- src/P2_Doubler.java
+++ src/P2_Doubler.java
@@ -7,7 +7,10 @@
 public class P2_Doubler {
 
 	public static void main(String[] args) {
-
+		int x = 9;
+		
+		System.out.println("What is your number? " + x);
+		System.out.println("That number doubled is " + (x * 2) + ".");
 	}
 
 }
